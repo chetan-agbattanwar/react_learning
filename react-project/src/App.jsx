922a1411c322a2287e63de1491f811820736f16f
chetan <chetanagbattanwar26@gmail.com>
2024-06-09 13:19:31 +0530

    loader

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -2,13 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Day1 from './Components/Day1'
+import MyLoader from './Components/loader/MyLoader'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <Day1/>
+    <MyLoader/>
   )
 }
 
---
+++ react-project/src/Components/loader/MyLoader.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './style.css'
+
+function MyLoader() {
+  return (
+    <div className='my-loader'/>
+  )
+}
+
+export default MyLoader
