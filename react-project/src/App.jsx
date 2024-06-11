0c93c7944f10c78670957ec9d38d6750a60bfb88
chetan <chetanagbattanwar26@gmail.com>
2024-06-11 10:01:09 +0530

    adding css

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import BgChanger from './Components/background-color-changer/BgChanger'
+import Day1 from './Components/Day1'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <BgChanger/>
+    <Day1/>
   )
 }
 
--- react-project/src/Components/Day1.jsx
+++ react-project/src/Components/Day1.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import './style.css'
+
+let style = {color:"orange", backgroundColor:"black"}
 
 function Day1() {
   return (
-    <div>Day1</div>
+    <>
+    <h1 style={{color:"red", backgroundColor:"black"}}>inline css</h1>
+    <h2 style={style}>internal css</h2>
+    <h3>external css</h3>
+    </>
   )
 }
 
