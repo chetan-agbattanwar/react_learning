a113125aa390b65573c625b39516d1b14db48c04
chetan <chetanagbattanwar26@gmail.com>
2024-06-16 14:44:08 +0530

    context api example2 and deleted example1 +

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,8 +1,6 @@
 import './App.css'
 import ParentComponent from './Components/ContextAPI/UseContext/Example2/ParentComponent'
 
-export const myContext = createContext()
-
 function App() {
 
   return (
